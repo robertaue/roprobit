033ff3c353dca445aefa9749f7ab299dd3c3afb7
Robert Aue <robert.aue@hotmail.de>
2019-06-18 12:15:56 +0200

    Add 95% CI

--- R/summary.roprobit.R
+++ R/summary.roprobit.R
@@ -6,6 +6,8 @@
                     se=sqrt(diag(x$vcov)))
    su$t <- su$coef/su$se
    su$p <- 2*(1 - pnorm(abs(su$t)))
+   su[['2.5%']]  <- x$beta.ci95[1,]
+   su[['97.5%']] <- x$beta.ci95[2,]
    
    print(su)
  }
